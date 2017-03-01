41ca8c711c7ffb900874bf113ec9bda2b84ed990
dpei <peidong2009@gmail.com>
2017-03-01 09:42:04 -0700

    Add files via upload

---
+++ convertGray.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of makeGray here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import edu.duke.*;
+import java.io.*;
+public class convertGray{
+    public ImageResource makeGray(ImageResource inImage){
+        ImageResource outImage = new ImageResource(inImage.getWidth(), inImage.getHeight());
+        for (Pixel pixel: outImage.pixels()){
+            Pixel inPixel = inImage.getPixel(pixel.getX(),pixel.getY());
+            int avg = (inPixel.getGreen()+inPixel.getRed()+inPixel.getBlue())/3;
+            pixel.setRed(avg);
+            pixel.setBlue(avg);
+            pixel.setGreen(avg);
+        }
+        return outImage;
+    }
+    /* Test for one simple case */
+    
+    public void testGray(){
+        ImageResource ir = new ImageResource();
+        ImageResource gray = makeGray(ir);
+        gray.draw();
+    }
+    /**This command could print the file name
+    * System.out.println(f); when you have a void, you have a method to 
+    * be called in the object you created. Otherwise no.
+    */    
+    public void saveGray(){
+        DirectoryResource dr = new DirectoryResource();
+        for (File f: dr.selectedFiles()){
+            ImageResource inImage = new ImageResource(f);
+            ImageResource outImage = makeGray(inImage);
+            String inName = inImage.getFileName();
+            String outName = "gray-" + inName;
+            outImage.setFileName(outName);
+            outImage.draw();
+            outImage.save();
+        }
+    }
+}
+
