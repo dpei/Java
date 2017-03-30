7e8b9a919ffd34789ea793a10d37ede5b7cf497c
dpei <peidong2009@gmail.com>
2017-03-30 10:53:07 -0500

    Add files via upload

--- AnalyzeLog/LogAnalyzer.java
+++ AnalyzeLog/LogAnalyzer.java
@@ -16,7 +16,7 @@ public class LogAnalyzer
      public LogAnalyzer() {
          records = new ArrayList<LogEntry>();
      }
-        
+     
      public void readFile(String filename) {
          FileResource fi = new FileResource(filename);
          for (String s : fi.lines()){
@@ -25,11 +25,32 @@ public class LogAnalyzer
          }
      }
         
+     public int countUniqueIPs(){
+         ArrayList<String> uniqueIPs = new ArrayList<String>();
+         for (LogEntry record : records){
+             String IP = record.getIpAddress();
+             if (!uniqueIPs.contains(IP)){
+                 uniqueIPs.add(IP);
+             }
+         }
+         return uniqueIPs.size();
+     }
+     
+     public int uniqueIpOnDay(String someday){
+         ArrayList<String> uniqueIPs = new ArrayList<String>();
+         for (LogEntry record : records){
+             String date = record.getAccessTime().toString();
+             String IP = record.getIpAddress();
+             if (date.indexOf(someday)!=-1 && uniqueIPs.indexOf(IP)==-1){
+                 uniqueIPs.add(IP);
+             }
+         }
+         return uniqueIPs.size();
+     }
+     
      public void printAll() {
          for (LogEntry le : records) {
              System.out.println(le);
          }
      }
-     
-     
 }
