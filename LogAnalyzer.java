cfda6b9b431e8e6b96c9329c93f0600c7dd48446
dpei <peidong2009@gmail.com>
2017-03-30 08:59:56 -0500

    Add files via upload

---
+++ LogAnalyzer.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class LogAnalyzer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+import edu.duke.*;
+
+public class LogAnalyzer
+{
+     private ArrayList<LogEntry> records;
+     
+     public LogAnalyzer() {
+         records = new ArrayList<LogEntry>();
+     }
+        
+     public void readFile(String filename) {
+         FileResource fi = new FileResource(filename);
+         for (String s : fi.lines()){
+             LogEntry slog = WebLogParser.parseEntry(s);
+             records.add(slog);
+         }
+     }
+        
+     public void printAll() {
+         for (LogEntry le : records) {
+             System.out.println(le);
+         }
+     }
+     
+     
+}
