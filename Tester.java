8fec7b544f5bdbbc0649aa310e61b11c0fe17430
dpei <peidong2009@gmail.com>
2017-03-31 08:43:22 -0500

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,35 @@
+
+/**
+ * Tester class was written to test the code provide by the Duke team.
+ * Also let Dong get familar with the code.
+ * 
+ * @author Dong Pei
+ * @created Mar. 31. 2016
+ */
+
+
+import edu.duke.*;
+
+
+public class Tester {
+    public void testCaesarCipher(){
+        FileResource fi = new FileResource();
+        String Msg = fi.asString();
+        CaesarCipher cc = new CaesarCipher(1);
+        String enMsg = cc.encrypt(Msg);
+        System.out.println("the encrypted message is: "+ enMsg);
+        System.out.println("the decrypted message is: "+ cc.decrypt(enMsg));
+    }
+    public void testCaesarCracker(){
+        FileResource fi = new FileResource("VigenereTestData/titus-small_key5.txt");
+        String enMsg = fi.asString();
+        CaesarCracker ck = new CaesarCracker();
+        System.out.println("the decrypted message is (English): "+ck.decrypt(enMsg));
+        // another test case
+        FileResource fi2 = new FileResource("VigenereTestData/oslusiadas_key17.txt");
+        String enMsg2 = fi2.asString();
+        CaesarCracker ck2 = new CaesarCracker('a');
+        System.out.println("the decrypted message is (Portuguese): "+ck2.decrypt(enMsg2));
+        
+    }
+}
