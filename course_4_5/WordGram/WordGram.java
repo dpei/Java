1a33132c649d0e14d7c216d8458979f053cf2587
dpei <peidong2009@gmail.com>
2017-04-17 21:54:11 -0500

    Add files via upload

---
+++ course_4_5/WordGram/WordGram.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+public class WordGram {
+    private String[] myWords;
+    private int myHash;
+
+    public WordGram(String[] source, int start, int size) {
+        myWords = new String[size];
+        System.arraycopy(source, start, myWords, 0, size);
+    }
+
+    public String wordAt(int index) {
+        if (index < 0 || index >= myWords.length) {
+            throw new IndexOutOfBoundsException("bad index in wordAt "+index);
+        }
+        return myWords[index];
+    }
+
+    public int length(){
+        return myWords.length;
+    }
+
+    // println automatically calls toString() even if not specified.
+    public String toString(){
+        String ret = "";
+        for (int i=0; i<myWords.length; i++){
+            ret = ret+myWords[i]+" ";
+        }
+        return ret.trim();
+    }
+
+    public boolean equals(Object o) {
+        WordGram other = (WordGram) o;
+        if (this.length() != other.length()) {
+            return false;
+        }
+        for (int i=0; i < myWords.length; i++){
+            if (!myWords[i].equals(other.wordAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public WordGram shiftAdd(String word) { 
+        WordGram out = new WordGram(myWords, 0, myWords.length);
+        for (int i=0; i<myWords.length; i++){
+            out.wordAt(i) = myWords.wordAt(i+1);
+        }
+        System.arraycopy(source, start, myWords, 0, size);
+        out[myWords.length] = word;
+        return out;
+    }
+
+}
